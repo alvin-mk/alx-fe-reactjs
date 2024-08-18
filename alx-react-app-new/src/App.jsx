30839ea3e013e8593176f5933dc34aa9cd5dcbea
Alvin K <ak.mbuthia98@gmail.com>
2024-08-18 13:25:40 +0400

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -9,6 +9,23 @@ import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
 import { Counter } from './components/Counter'
 
+/////
+import React from 'react';
+import UserContext from './UserContext';
+import ProfilePage from './ProfilePage';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
+////
 function App() {
   const [count, setCount] = useState(0)
 
