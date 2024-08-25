40c293afac64390c4a77ec655afd5d125a9c6727
Alvin K <ak.mbuthia98@gmail.com>
2024-08-25 12:25:22 +0400

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import useRecipeStore from './recipeStore';
 
 const RecipeList = () => {
@@ -8,7 +9,10 @@ const RecipeList = () => {
     <div>
       {filteredRecipes.map((recipe) => (
         <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
+          <h3>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link> 
+            {/* Wrap the title with Link to enable navigation */}
+          </h3>
           <p>{recipe.description}</p>
         </div>
       ))}
