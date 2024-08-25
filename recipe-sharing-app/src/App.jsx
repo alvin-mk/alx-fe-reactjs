d9cba77360fc6d895078732863d121af324b6c0b
Alvin K <ak.mbuthia98@gmail.com>
2024-08-25 11:48:14 +0400

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -41,7 +41,7 @@ function App() {
         </p>
         
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs"> 
         Click on the Vite and React logos to learn more
       </p>
     </>
