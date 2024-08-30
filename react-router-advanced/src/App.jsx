f28db4aa7e015740f1ee07c042dd9a23d2642ece
Alvin K <ak.mbuthia98@gmail.com>
2024-08-30 16:01:16 +0400

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Profile from './components/Profile';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Profile from './Profile';
+import Home from './Home';
+import BlogPost from './BlogPost';  // Import the BlogPost component
 
 function App() {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+          <li><Link to="/blog/1">Blog Post 1</Link></li>  {/* Example link to a dynamic blog post */}
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/blog/:id" element={<BlogPost />} />  {/* Dynamic route for blog posts */}
       </Routes>
     </Router>
   );
