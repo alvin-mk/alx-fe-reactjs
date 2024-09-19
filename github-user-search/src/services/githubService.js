75e17c565d7cc79c2c60c8d1e259a45a67407b1c
Alvin K <ak.mbuthia98@gmail.com>
2024-09-19 18:37:21 +0400

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,22 +1,12 @@
 import axios from 'axios';
 
-// Define the base URL for GitHub API
-const GITHUB_API_URL = "https://api.github.com/search/users?q";
+const BASE_URL = 'https://api.github.com/users';
 
-// Function to fetch users based on search criteria
-export const fetchUsers = async (searchTerm, location = '', minRepos = 0) => {
+export const fetchUserData = async (username) => {
   try {
-    // Construct the query string with optional parameters
-    const query = `${searchTerm}${location ? `+location:${location}` : ''}${minRepos ? `+repos:${minRepos}` : ''}`;
-    const url = `${GITHUB_API_URL}?q=${query}`;
-
-    // Make the API request
-    const response = await axios.get(url);
-
-    // Return the results from the API response
-    return response.data.items;
+    const response = await axios.get(`${BASE_URL}/${username}`);
+    return response.data;
   } catch (error) {
-    // Handle and throw errors to be caught in the component
-    throw new Error(error.response ? error.response.data.message : 'Error fetching user data');
+    throw new Error('User not found');
   }
 };
